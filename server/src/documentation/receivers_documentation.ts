c6cd42292d583f25b8d655a05ca4eaaad6182a54
Petr Makhnev <51853996+i582@users.noreply.github.com>
2025-02-15 00:53:40 +0400

    feat(documentation): show use documentation for receiver and init  functions (#227)
    Fixes #190

--- server/src/documentation/receivers_documentation.ts
+++ server/src/documentation/receivers_documentation.ts
@@ -1,17 +1,19 @@
 import type {InitFunction, MessageFunction} from "@server/psi/Decls"
 import {Expression} from "@server/psi/Node"
 import {BouncedTy, MessageTy} from "@server/types/BaseTy"
+import {extractCommentsDoc} from "@server/documentation/documentation"
 
 const CODE_FENCE = "```"
 
 export function generateInitDoc(func: InitFunction): string | null {
     const name = func.nameLike()
+    const codeDoc = extractCommentsDoc(func)
 
     const doc = `Constructor function \`init()\` runs on deployment of the contract.
 If a contract has any persistent state variables without default values specified, it must initialize them in this function.`
 
     const link = "https://docs.tact-lang.org/book/contracts/#init-function"
-    return defaultResult(name, doc, link)
+    return defaultResult(name, codeDoc, doc, link)
 }
 
 export function generateReceiverDoc(func: MessageFunction): string | null {
@@ -19,6 +21,8 @@ export function generateReceiverDoc(func: MessageFunction): string | null {
 
     const kind = func.kindIdentifier()?.text
     if (!kind) return null
+
+    const codeDoc = extractCommentsDoc(func)
 
     const link =
         kind === "receive"
@@ -32,15 +36,14 @@ export function generateReceiverDoc(func: MessageFunction): string | null {
     const param = func.parameter()
     if (!param) {
         const doc = "Called when an empty message is sent to the contract"
-        return defaultResult(name, doc, link)
+        return defaultResult(name, codeDoc, doc, link)
     }
 
     // receive("message") {}
     //         ^^^^^^^^^
     if (param.type === "string") {
-        const doc =
-            'Called when a text message with a specific comment is sent to the contract (maximum "message" length is 123 bytes)'
-        return defaultResult(name, doc, link)
+        const doc = `Called when a text message ${param.text} is sent to the contract (maximum "message" length is 123 bytes)`
+        return defaultResult(name, codeDoc, doc, link)
     }
 
     const typeNode = param.childForFieldName("type")
@@ -52,37 +55,38 @@ export function generateReceiverDoc(func: MessageFunction): string | null {
     //              ^^^^^^
     if (type.name() === "String") {
         const doc = "Called when an arbitrary text message is sent to the contract"
-        return defaultResult(name, doc, link)
+        return defaultResult(name, codeDoc, doc, link)
     }
 
     // receive(msg: ChangeOwner) {}
     //              ^^^^^^^^^^^
     if (type instanceof MessageTy) {
         const doc = `Called when a binary message of type \`${type.name()}\` is sent to the contract`
-        return defaultResult(name, doc, link)
+        return defaultResult(name, codeDoc, doc, link)
     }
 
     // bounced(msg: bounced<ChangeOwner>) {}
     //              ^^^^^^^^^^^^^^^^^^^^
     if (type instanceof BouncedTy) {
         const doc = `Called when a binary message of type \`${type.innerTy.name()}\` is sent to the contract`
-        return defaultResult(name, doc, link)
+        return defaultResult(name, codeDoc, doc, link)
     }
 
     // external(msg: Slice) {}
     //               ^^^^^
     if (type.name() === "Slice") {
         const doc = "Called when binary message of unknown type is sent to the contract"
-        return defaultResult(name, doc, link)
+        return defaultResult(name, codeDoc, doc, link)
     }
 
-    return defaultResult(name, "", link)
+    return defaultResult(name, codeDoc, "", link)
 }
 
-function defaultResult(name: string, doc: string, link: string): string {
+function defaultResult(name: string, codeDoc: string, doc: string, link: string): string {
     return (
         `${CODE_FENCE}tact\n${name} {}\n${CODE_FENCE}` +
         "\n" +
+        (codeDoc.length > 0 ? `${codeDoc}\n\n----\n` : "") +
         doc +
         "\n\nLearn more in documentation: " +
         link
