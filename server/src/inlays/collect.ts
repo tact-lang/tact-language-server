26d06e577b4e8cfa82e2ca1a405f89ba79bb49b8
i582 <51853996+i582@users.noreply.github.com>
2025-02-15 20:58:13 +0400

    feat(inlay-hints): show stack info for ASM instruction (disabled by default)
    Fixes #91

--- server/src/inlays/collect.ts
+++ server/src/inlays/collect.ts
@@ -47,11 +47,25 @@ function processParameterHints(
     }
 }
 
-function getGasPresentation(gas: string | undefined, format: string): string {
+function getStackPresentation(rawStack: string | undefined): string {
+    if (!rawStack) return ""
+    const prefix = rawStack.startsWith("-") ? "∅ " : ""
+    const stack = prefix + rawStack.replace("-", "→")
+    if (stack !== "") {
+        return `(${stack})`
+    }
+    return ""
+}
+
+function instructionPresentation(
+    gas: string | undefined,
+    stack: string | undefined,
+    format: string,
+): string {
     if (!gas || gas === "") {
         return ": no data"
     }
-    return format.replace("{gas}", gas)
+    return format.replace("{gas}", gas).replace("{stack}", getStackPresentation(stack))
 }
 
 function calculatePushcontGas(instr: AsmInstr, file: File): GasConsumption | null {
@@ -293,7 +307,7 @@ export function collect(
 
                 result.push({
                     kind: InlayHintKind.Type,
-                    label: getGasPresentation(gas.value.toString(), hints.gasFormat),
+                    label: instructionPresentation(gas.value.toString(), "", hints.gasFormat),
                     position: {
                         line: openBrace.endPosition.row,
                         character: openBrace.endPosition.column,
@@ -302,11 +316,15 @@ export function collect(
                 })
             }
 
-            const gasPresentation = getGasPresentation(info?.doc.gas, hints.gasFormat)
+            const presentation = instructionPresentation(
+                info?.doc.gas,
+                info?.doc.stack,
+                hints.gasFormat,
+            )
 
             result.push({
                 kind: InlayHintKind.Type,
-                label: gasPresentation,
+                label: presentation,
                 position: {
                     line: n.endPosition.row,
                     character: n.endPosition.column,
