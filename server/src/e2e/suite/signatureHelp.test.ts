daa8d27164bd9bf89bb82c49274e347ac579cec0
xpyctumo <30053565+xpyctumo@users.noreply.github.com>
2025-02-17 23:15:26 +0300

    fix(test): remove debug info and fix test

--- server/src/e2e/suite/signatureHelp.test.ts
+++ server/src/e2e/suite/signatureHelp.test.ts
@@ -44,8 +44,6 @@ suite("Signatures Test Suite", () => {
         protected runTest(testFile: string, testCase: TestCase): void {
             test(`Signature: ${testCase.name}`, async () => {
                 const signature = await this.getSignature(testCase.input)
-                console.log("signature")
-                console.log(JSON.stringify(signature))
                 const items = signature.signatures.map(item => {
                     const label = item.label
                     if (item.activeParameter !== undefined) {
