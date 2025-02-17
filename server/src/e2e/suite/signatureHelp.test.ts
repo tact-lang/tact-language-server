1e0712052131166b0b6697b2b4e7cd5eb48598fa
xpyctumo <30053565+xpyctumo@users.noreply.github.com>
2025-02-17 17:05:43 +0300

    fix(test): trying to pass windows tests

--- server/src/e2e/suite/signatureHelp.test.ts
+++ server/src/e2e/suite/signatureHelp.test.ts
@@ -18,6 +18,8 @@ suite("Signatures Test Suite", () => {
             this.editor.selection = new vscode.Selection(position, position)
             this.editor.revealRange(new vscode.Range(position, position))
 
+            await this.editor.document.save()
+
             return vscode.commands.executeCommand<vscode.SignatureHelp>(
                 "vscode.executeSignatureHelpProvider",
                 this.document.uri,
