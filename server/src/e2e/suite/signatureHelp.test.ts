c6df9e81a37385ab70210d5432482b2ecfca9335
Petr Makhnev <51853996+i582@users.noreply.github.com>
2025-02-18 04:10:19 +0400

    fix(signature-help): for struct init inside function call (#259)
    Fixes #252

--- server/src/e2e/suite/signatureHelp.test.ts
+++ server/src/e2e/suite/signatureHelp.test.ts
@@ -5,53 +5,41 @@ import type {TestCase} from "./TestParser"
 
 suite("Signatures Test Suite", () => {
     const testSuite = new (class extends BaseTestSuite {
-        public async getSignature(input: string): Promise<vscode.SignatureHelp> {
-            await this.editor.edit(builder => {
-                const fullRange = new vscode.Range(
-                    this.document.lineAt(0).range.start,
-                    this.document.lineAt(this.document.lineCount - 1).range.end,
-                )
-                builder.delete(fullRange)
-                builder.insert(this.document.positionAt(0), input)
-            })
+        public async getSignatureHelp(input: string): Promise<vscode.SignatureHelp | undefined> {
+            const textWithoutCaret = input.replace("<caret>", "")
+            await this.replaceDocumentText(textWithoutCaret)
 
-            const editorText = this.document.getText()
-            const caretIndex = editorText.indexOf("<caret>")
-
+            const caretIndex = input.indexOf("<caret>")
             if (caretIndex === -1) {
                 throw new Error("No <caret> marker found in input")
             }
 
-            const caretPosition = this.document.positionAt(caretIndex)
-
-            await this.editor.edit(builder => {
-                const caretRange = this.document.getWordRangeAtPosition(caretPosition, /<caret>/)
-                if (caretRange) {
-                    builder.delete(caretRange)
-                }
-            })
-
-            this.editor.selection = new vscode.Selection(caretPosition, caretPosition)
-            this.editor.revealRange(new vscode.Range(caretPosition, caretPosition))
+            const position = this.document.positionAt(caretIndex)
+            this.editor.selection = new vscode.Selection(position, position)
+            this.editor.revealRange(new vscode.Range(position, position))
 
             return vscode.commands.executeCommand<vscode.SignatureHelp>(
                 "vscode.executeSignatureHelpProvider",
                 this.document.uri,
-                caretPosition,
+                position,
             )
         }
 
         protected runTest(testFile: string, testCase: TestCase): void {
             test(`Signature: ${testCase.name}`, async () => {
-                const signature = await this.getSignature(testCase.input)
-                const items = signature.signatures.map(item => {
-                    const label = item.label
-                    if (item.activeParameter !== undefined) {
-                        const activeParamLabel = item.parameters[item.activeParameter]?.label ?? ""
-                        return `${activeParamLabel.toString()}\n${label}`
-                    }
-                    return ""
-                })
+                const signature = await this.getSignatureHelp(testCase.input)
+                const items =
+                    signature === undefined
+                        ? ["no signature help"]
+                        : signature.signatures.map(item => {
+                              const label = item.label
+                              if (item.activeParameter !== undefined) {
+                                  const activeParamLabel =
+                                      item.parameters[item.activeParameter]?.label ?? ""
+                                  return `${activeParamLabel.toString()}\n${label}`
+                              }
+                              return ""
+                          })
                 const expected = testCase.expected.split("\n").filter((line: string) => line !== "")
                 if (BaseTestSuite.UPDATE_SNAPSHOTS) {
                     this.updates.push({
@@ -75,5 +63,5 @@ suite("Signatures Test Suite", () => {
     teardown(async () => testSuite.teardown())
     suiteTeardown(() => testSuite.suiteTeardown())
 
-    testSuite.runTestsFromDirectory("signatures")
+    testSuite.runTestsFromDirectory("signatureHelp")
 })
