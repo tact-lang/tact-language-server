8dcfe10af992fed110797af5b28d32e2c4c42a37
i582 <51853996+i582@users.noreply.github.com>
2025-03-10 22:36:34 +0400

    fix

--- server/src/e2e/suite/index.ts
+++ server/src/e2e/suite/index.ts
@@ -14,7 +14,7 @@ export async function run(): Promise<void> {
     const testsRoot = path.resolve(__dirname, ".")
 
     return new Promise((resolve, reject) => {
-        glob("documentation.test.js", {
+        glob("*.test.js", {
             cwd: testsRoot,
         })
             .then(files => {
