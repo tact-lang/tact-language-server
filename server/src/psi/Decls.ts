d6d6b89fa6e17bc907fbd37dbb8090d1b8891d15
i582 <51853996+i582@users.noreply.github.com>
2025-04-03 16:06:13 +0400

    fix(documentation): show lazy_deployment_bit in TL-B for contracts
    Fixes #544

--- server/src/psi/Decls.ts
+++ server/src/psi/Decls.ts
@@ -119,6 +119,12 @@ export class StorageMembersOwner extends NamedNode {
             .map(value => new Fun(value, this.file))
     }
 
+    public parametersNode(): undefined | SyntaxNode {
+        const params = this.node.childForFieldName("parameters")
+        if (!params) return undefined
+        return params
+    }
+
     public parameters(): Field[] {
         const params = this.node.childForFieldName("parameters")
         if (!params) return []
@@ -239,7 +245,18 @@ export class StorageMembersOwner extends NamedNode {
 
 export class Trait extends StorageMembersOwner {}
 
-export class Contract extends StorageMembersOwner {}
+export class Contract extends StorageMembersOwner {
+    public hasLazyInitializationBit(): boolean {
+        const hasParameters = this.parametersNode() !== undefined
+        if (hasParameters) {
+            // if contract has parameters (even empty), lazy bit is not used
+            return false
+        }
+
+        // and in any other cases we have lazy bit
+        return true
+    }
+}
 
 export class InitFunction extends Node {
     public nameLike(): string {
